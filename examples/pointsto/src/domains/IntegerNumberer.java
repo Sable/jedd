9259a20b8eba8ad05f9a6b948592a203c1e15162
Ondrej Lhoták <olhotak@uwaterloo.ca>
2004-07-20 15:14:16 +0000

    update Jedd examples for 0.2 changes

--- examples/pointsto/src/domains/IntegerNumberer.java
+++ examples/pointsto/src/domains/IntegerNumberer.java
@@ -4,11 +4,11 @@ import jedd.*;
 public class IntegerNumberer implements Numberer {
     public void add( Object o ) {
     }
-    public Object get( int number ) {
-        return new Integer(number);
+    public Object get( long number ) {
+        return new Long(number);
     }
-    public int get( Object o ) {
-        return ((Integer) o).intValue();
+    public long get( Object o ) {
+        return ((Long) o).intValue();
     }
     public int size() { return 1000000; }
     public static IntegerNumberer v() { return instance; }
